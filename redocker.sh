d6d4585e480f805a20a0055afff698b2acb520d3
JerryGu <jerrygu.gjw@gmail.com>
2022-12-27 17:13:29 +0800

    fix docker communication

--- redocker.sh
+++ redocker.sh
@@ -1,4 +1,4 @@
 docker rm -f back
 docker rmi vistaweb-backend:v1
 docker build . -t vistaweb-backend:v1
-docker run --restart=unless-stopped -p 8181:8181 --name back --link vistasql:localhost -d vistaweb-backend:v1
+docker run --restart=unless-stopped -p 8181:8181 --name back --link vistasql:db -d vistaweb-backend:v1
