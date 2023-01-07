f7476297335e7bf7616121073f1ac6605eaa01b0
Jerry Gu <jerrygu.gjw@gmail.com>
2023-01-07 14:23:12 +0800

    fix

--- router_handler/commonapi.js
+++ router_handler/commonapi.js
@@ -4,14 +4,11 @@ const config = require('../config')
 
 exports.getimg = (req, res) => {
     const sql = `select base64 from img_info where id=?`
-    db.query(sql, req.body.id, (err, results) => {
+    db.query(sql, req.query.id, (err, results) => {
         if (err) {
           return res.cc(err)
         }
         if (results.length !== 1) return res.cc('获取用户信息失败！')
-        res.send({
-          status: true,
-          base64 : results[0].base64,
-        })
+        res.send(results[0].base64)
       })
   }
