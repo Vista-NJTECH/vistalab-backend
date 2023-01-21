4f1b0ad2355fe660f9e358a1e7d6429e414a4279
JerryGu <jerrygu.gjw@gmail.com>
2023-01-21 12:51:39 +0800

    update Spring Festival activity

--- routes_handlers/feedback.js
+++ routes_handlers/feedback.js
@@ -1,25 +1,39 @@
 const db = require('../db/index')
 
-exports.submit = (req, res) => {
+exports.submit = async (req, res) => {
   const {feedback} = req.body
-  if(feedback == "sign") return res.cc("/public/ctf/html/sign.html", true)
+  if(feedback == "sign") return res.cc("https://backend.vistalab.top/public/ctf/html/sign.html", true)
   if(feedback == "secondkey") return res.cc("/public/ctf/html/flag2.html", true)
-  if(feedback == "loveu") return res.cc("bacon:AABABABBBABAAABAABBAAABAABAABBABBAAAABAA", true)
-
-  if(feedback == "") return res.cc("不能为空!")
+  if(feedback == "flag3flag") return res.cc("BaconCipher : AABABABBBABAAABAABBAAABAABAABBABBAAAABAA   key : AABBAABBABABBABAAABB", true)
+  if(feedback == "good") { 
+    res.setHeader("png", "http://139.196.138.115:8081/public/pngpng.png")
+    return res.cc("success!QAQ-header!", true)
+  }
+  if(feedback == "byebye") { 
+    const sql = 'insert into feedback set ?'
+    await db.query(sql, { 
+      feedback: "通关一人",
+    }, function (err, noresults) {
+        if (err){
+          return res.cc(err)
+        } 
+    })
+    return res.cc("Congratulations! Very simple!", true)
+  }else
+  {if(feedback == "") return res.cc("不能为空!")
 
   const sql = 'insert into feedback set ?'
-      db.query(sql, { 
-        feedback: feedback,
-      }, function (err, noresults) {
-          if (err){
-            return res.cc(err)
-          } 
-          res.send({
-              status: true,
-              message: '提交成功',
-              })
-      })
+  db.query(sql, { 
+    feedback: feedback,
+  }, function (err, noresults) {
+      if (err){
+        return res.cc(err)
+      } 
+      res.send({
+          status: true,
+          message: '提交成功',
+          })
+  })}
 }
 
 exports.getAll = (req, res) => {
